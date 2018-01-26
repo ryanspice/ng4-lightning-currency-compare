6254c893a908fd3558635be65f7d4d72397881db
Ryan Spice <spice.ryan@hotmail.com>
2018-01-25 22:43:57 -0500

    Commenting/Cleaning select.ts

--- src/components/select.ts
+++ src/components/select.ts
@@ -4,51 +4,27 @@ import { ChangeDetectorRef } from '@angular/core';
 import {FixerService} from "../data/fixer.service";
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
-import {Component, NgModule,Input,ComponentFactory,ComponentRef, ComponentFactoryResolver, ViewContainerRef,  TemplateRef, ViewChild,  Output, EventEmitter} from '@angular/core'
+import {Component, Input,       Output, EventEmitter} from '@angular/core'
 @Component({
   selector: 'select-lightning',
   templateUrl: './select.html',
   styleUrls: ['./select.scss']
 })
 export class SelectLightning {
-	title = 'Currency Converter!';
+
+	@Output() output = new EventEmitter();
 
 	fixer:FixerService;
+	private countries:Array<any>;
 
-	  @Input() type: string = "success";
-	  @Output() output = new EventEmitter();
+	constructor(private http: Http){}
 
-	latest:any;
-	@Input() public countries;
+	/* Initialize service then grab data */
 
+	async ngOnInit(){
 
-	constructor(private http: Http,private cd: ChangeDetectorRef){
-
-		(this.fixer = new FixerService(http));
-
-		/*
-		this.latest = (this.fixer.retrieveStore('latest'));
-		this.latest.subscribe(async (data:any)=>{
-
-			this.countries= Object.keys(JSON.parse(data._body).rates);
-			this.cd.markForCheck();
-	   })
-		this.latest.subscribe(data => this.countries = (this.fixer.setStore(data)));
-		*/
-
-	   //})
-
-	   this.init();
-	}
-
-	async init(){
-
-			   this.latest = this.fixer.retrieveStore('latest', (data)=>{ this.countries = this.fixer.getCountries(data); });
-
-			   //console.log(this, FixerService.store)
-
-			   //await this.fixer.getCountries();
-
+		this.fixer = await new FixerService(this.http);
+		await this.fixer.retrieveStore('latest', (data)=>{ this.countries = this.fixer.getCountries(data); });
 
 	}
 
